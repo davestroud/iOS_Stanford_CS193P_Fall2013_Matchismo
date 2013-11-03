3cff07ccc6d1fb265e660f44e6d632397ca1f166
Herko ter Horst <herko@terhorst.net>
2013-11-03 13:01:53 +0100

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Herko ter Horst on 03/11/13.
+//  Copyright (c) 2013 Herko ter Horst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Herko ter Horst on 03/11/13.
+//  Copyright (c) 2013 Herko ter Horst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
